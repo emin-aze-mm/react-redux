44ec9271288a2097c2fb36a442b5482f73a949f3
emin-aze-mm <emin.mammadzada.2021@gmail.com>
2022-05-25 16:29:58 +0400

    added

---
+++ src/redux/actions.js
@@ -0,0 +1,18 @@
+import { ADD_GOOD_TO_CART, REMOVE_GOOD_TO_CART } from "../redux/action-type";
+export function addGoodToCart(id) {
+  return {
+    type: ADD_GOOD_TO_CART,
+    payload: {
+      id: id,
+    },
+  };
+}
+
+export function removeGoodToCart(id) {
+  return {
+    type: REMOVE_GOOD_TO_CART,
+    payload: {
+      id: id,
+    },
+  };
+}
